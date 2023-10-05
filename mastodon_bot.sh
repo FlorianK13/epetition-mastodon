14e4daa581323e8a9d22f8fa95a1eda8039235e9
Florian Kotthoff <florian.kotthoff@posteo.de>
2023-10-05 17:45:52 +0200

    Revert "Change comments"
    This reverts commit b0935d99fc7112aaf96db5a51fae1f42abbfd948.

--- mastodon_bot.sh
+++ mastodon_bot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LOG_FILE="/home/flo/mastobot/mastodon_bot.log"
-cd mastobot && /home/flo/miniconda3/envs/mastobot/bin/python mastodon_bot.py >> "$LOG_FILE" 2>&1
+LOG_FILE="mastodon_bot.log"
+python mastodon_bot.py >> "$LOG_FILE" 2>&1
